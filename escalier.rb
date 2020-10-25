ea1d9a31672131fd4320d309a2076c4675c52f90
SalahHW <salah.bouheniche@gmail.com>
2020-10-25 17:54:55 +0100

    Création du programme escalier.rb

---
+++ escalier.rb
@@ -0,0 +1,19 @@
+if ARGV.length < 1
+    puts "#{__FILE__} require at least one parameter."
+    exit
+elsif ARGV.length > 1
+    puts "#{__FILE__} require only one parameter."
+    exit
+end
+
+number_of_steps = ARGV[0].to_i
+puts "Your stair will have #{number_of_steps} steps :"
+
+line = 1
+while line <= number_of_steps
+    number_of_space = number_of_steps - line
+    number_of_space.times {print " "}
+    line.times {print "#"}
+    puts " "
+    line += 1
+end
