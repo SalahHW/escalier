a2247c7cb6096d5fb7fbb3c9ad74dfcab904a1da
SalahHW <salah.bouheniche@gmail.com>
2020-10-25 20:38:45 +0100

    working program  majuscule.rb

--- majuscule.rb
+++ majuscule.rb
@@ -5,3 +5,17 @@ elsif ARGV.length > 1
     puts "#{__FILE__} require only one parameter. A string"
     exit
 end
+
+origin_phrase = ARGV[0].split("")
+
+origin_phrase.each_with_index do |el, i|
+    if origin_phrase[i] == " "
+        print " "
+    elsif i % 2 == 0
+        print origin_phrase[i].downcase
+    else
+        print origin_phrase[i].upcase
+    end
+end
+puts ""
+
